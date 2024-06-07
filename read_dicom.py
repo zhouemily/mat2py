ef236c2d15645d365776586d6a756ce1eb79951e
Emily Zhou <40789206+zhouemily@users.noreply.github.com>
2024-06-07 10:36:55 -0700

    add read_dicom test files
    run read_dicom scripts for test

---
+++ read_dicom.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import pydicom
+import numpy as np
+
+# Read the DICOM file
+dcm_file = 'd004/7T_STAB_12_8_2023.MR.SUTTON_SEQTESTING.0004.0256.2023.12.08.07.38.12.980291.186296403.IMA' 
+dicom = pydicom.dcmread(dcm_file)
+tmp = dicom.pixel_array
+
+# Print a portion of the pixel values (e.g., top-left 10x10 region)
+print('Python Pixel Values:')
+print(tmp[:10, :10])
+# Save the 100x100 region to a text file
+np.savetxt('python_pixels.txt', tmp[:100, :100], fmt='%d')
