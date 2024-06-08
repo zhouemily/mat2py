26f10e60de0891133a570b19d0ef83dc7244dd8a
Emily Zhou <40789206+zhouemily@users.noreply.github.com>
2024-06-08 11:46:43 -0700

    add test.sh script for testing

---
+++ test.sh
@@ -0,0 +1,19 @@
+echo "testcased for  stabcheck36_trio_CM2.py script"
+echo "show usage help menu:"
+./stabcheck36_trio_CM2.py -h
+
+echo "result and stability_history_2024.txt files are generated in ./logdir"
+echo "test with 64 random Dicom files with prefix: T_STAB_12_8_2023.MR.SUTTON_SEQTESTING.0004 "
+./stabcheck36_trio_CM2.py -d test_dir_d004/test_64_files
+echo "test with 128 random Dicom files with prefix: T_STAB_12_8_2023.MR.SUTTON_SEQTESTING.0004 "
+./stabcheck36_trio_CM2.py -d test_dir_d004/test_128_files
+echo "test with 256 random Dicom files with prefix: T_STAB_12_8_2023.MR.SUTTON_SEQTESTING.0004 "
+./stabcheck36_trio_CM2.py -d test_dir_d004/test_256_files
+echo "test with 64 random Dicom files with prefix: T_STAB_12_8_2023.MR.SUTTON_SEQTESTING.0005 "
+./stabcheck36_trio_CM2.py -d test_dir_d005/test_64_files
+echo "test with 128 random Dicom files with prefix: T_STAB_12_8_2023.MR.SUTTON_SEQTESTING.0005 "
+./stabcheck36_trio_CM2.py -d test_dir_d005/test_128_files
+echo "test with 256 random Dicom files with prefix: T_STAB_12_8_2023.MR.SUTTON_SEQTESTING.0005 "
+./stabcheck36_trio_CM2.py -d test_dir_d005/test_256_files
+echo "results and stability_history_2024.txt files are generated in ./logdir"
+
